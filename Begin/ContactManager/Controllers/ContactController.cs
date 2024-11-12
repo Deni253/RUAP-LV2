3343b8c2d45ead4a4f660bde9622560d64f6764a
mono-praksa-projekt-admin <denz243010@gmail.com>
2024-11-12 18:38:28 +0100

    Creating the Contact Models and Augment the Contact Controller

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using ContactManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Begin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        // GET: api/contact
+        [HttpGet]
+        public Contact[] Get()
+        {
+            return new Contact[]
+         {
+            new Contact
+            {
+                Id = 1,
+                Name = "Glenn Block"
+            },
+            new Contact
+            {
+                Id = 2,
+                Name = "Dan Roth"
+            }
+         };
+        }
+    }
+}
--- Begin/ContactManager/Program.cs
+++ Begin/ContactManager/Program.cs
@@ -3,13 +3,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+// Add Swagger services (must be done before calling builder.Build())
+builder.Services.AddEndpointsApiExplorer(); // Necessary for API endpoint discovery
+builder.Services.AddSwaggerGen(); // Adds Swagger generation services
+
+var app = builder.Build(); // Build the application
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -19,6 +22,16 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+// Enable Swagger middleware to serve the generated Swagger as a JSON endpoint
+app.UseSwagger();
+
+// Enable the Swagger UI middleware (the interactive UI for Swagger docs)
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // Makes the Swagger UI available at the root (optional)
+});
 
 app.MapControllerRoute(
     name: "default",
